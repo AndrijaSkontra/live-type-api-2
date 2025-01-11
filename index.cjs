3cfb532fde55dca869934268fa34b95ba05eb1a5
AndrijaSkontra <andrija.skontra@kodelab.hr>
2025-01-11 13:28:38 +0100

    remove: auth0 stuff

--- index.cjs
+++ index.cjs
@@ -1,12 +1,10 @@
+require("dotenv").config();
 const cors = require("cors");
 const express = require("express");
 const { Server } = require("socket.io");
 const { createServer } = require("http");
-require("dotenv").config();
 const gameScoreRouter = require("./app/routes/game-scores.routes.cjs");
 const handleSocketConnection = require("./app/services/socket.cjs");
-const { auth } = require("express-openid-connect");
-const { requiresAuth } = require("express-openid-connect");
 
 const PORT = 3000;
 const app = express();
@@ -19,27 +17,6 @@ app.use(
 );
 
 app.use("/game-scores", gameScoreRouter);
-
-const config = {
-  authRequired: false,
-  auth0Logout: true,
-  secret: "a long, randomly-generated string stored in env",
-  baseURL: "http://localhost:3000",
-  clientID: "2THf1ya9l7flTTigebL03AoSdzWYoqPg",
-  issuerBaseURL: "https://dev-c0fffp3fa1nbkbn6.us.auth0.com",
-};
-
-// auth router attaches /login, /logout, and /callback routes to the baseURL
-app.use(auth(config));
-
-// req.isAuthenticated is provided from the auth router
-app.get("/", (req, res) => {
-  res.send(req.oidc.isAuthenticated() ? "Logged in" : "Logged out");
-});
-
-app.get("/profile", requiresAuth(), (req, res) => {
-  res.send(JSON.stringify(req.oidc.user));
-});
 
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
